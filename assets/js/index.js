b91c3d06e97053460bc95cdad8d1f102177ef647
Wyatt C <wcastellan@yahoo.com>
2021-11-23 11:23:54 -0500

    fixed issue on javascript side with accepting changes to have 'find a meal' button working

--- assets/js/index.js
+++ assets/js/index.js
@@ -166,13 +166,9 @@ function saveMeal() {
     <h2 class="font-bold text-xl mb-2">${meal.strMeal}</h2>
     <img class="object-cover h-48 w-full" src="${meal.strMealThumb}" alt="${meal.strMeal}" />
     <div class="px-3 py-2 justify-center"> 
-<<<<<<< HEAD
          ${meal.strCategory ? `<p class="font-light text-md mb-2">${meal.strCategory}</p>` : ""}
          ${meal.strArea ? `<p class="font-light text-md mb-2">${meal.strArea}</p>` : ""}
          <button class="modal-open btn my-4 py-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Expand</button> 
-=======
-         <button class="btn modal-open my-4 py-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" id="one">Expand</button> 
->>>>>>> 64c1b4df1eed69f832aa5db4fa18f5826dd25f15
          </div>    
     </div>
     
@@ -181,49 +177,6 @@ function saveMeal() {
   modalBtn.addEventListener("click", toggleModal);
   }
 
-<<<<<<< HEAD
-=======
-// modal section
-var openmodal = document.querySelectorAll('.modal-open')
-    for (var i = 0; i < openmodal.length; i++) {
-      openmodal[i].addEventListener('click', function(event){
-    	event.preventDefault()
-    	toggleModal()
-      })
-    }
-    
-    const overlay = document.querySelector('.modal-overlay')
-    overlay.addEventListener('click', toggleModal)
-    
-    var closemodal = document.querySelectorAll('.modal-close')
-    for (var i = 0; i < closemodal.length; i++) {
-      closemodal[i].addEventListener('click', toggleModal)
-    }
-    
-    document.onkeydown = function(evt) {
-      evt = evt || window.event
-      var isEscape = false
-      if ("key" in evt) {
-    	isEscape = (evt.key === "Escape" || evt.key === "Esc")
-      } else {
-    	isEscape = (evt.keyCode === 27)
-      }
-      if (isEscape && document.body.classList.contains('modal-active')) {
-    	toggleModal()
-      }
-    };
-    
-    
-    function toggleModal (e) {
-      console.log(e.target.id);
-      const body = document.querySelector('body')
-      const modal = document.querySelector('.modal')
-      modal.classList.toggle('opacity-0')
-      modal.classList.toggle('pointer-events-none')
-      body.classList.toggle('modal-active')
-    
-    }
->>>>>>> 64c1b4df1eed69f832aa5db4fa18f5826dd25f15
 
 
 }
